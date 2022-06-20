0594eb33a8dea834d7faa25e06d05c1d47bd8e12
jongbin-choi <kooma1123@naver.com>
2022-06-20 23:10:59 +0900

    mysite4 spring

--- src/main/java/com/javaex/controller/UserController.java
+++ src/main/java/com/javaex/controller/UserController.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -74,7 +75,7 @@ public class UserController {
 	
 	//회원정보 수정(수정폼)
 	@RequestMapping(value="/user/modifyForm", method = {RequestMethod.GET, RequestMethod.POST})
-	public String modifyForm(HttpSession session) {
+	public String modifyForm(Model model, HttpSession session) {
 		System.out.println("UserController > modifyForm()");
 		
 		//세션가져오기
@@ -83,9 +84,8 @@ public class UserController {
 		//no값으로 세션 값 불러오기
 		int no = authUser.getNo();
 		UserVo userModify = userService.getUserModify(no); 
-		
-		//가져온정보 다른 세션에 저장
-		session.setAttribute("user", userModify);
+		//가져온정보 Model에 저장
+		model.addAttribute("user",userModify);
 		
 		return "user/modifyForm";
 	}
@@ -97,16 +97,19 @@ public class UserController {
 		
 		//세션불러오기
 		UserVo authUser = (UserVo)session.getAttribute("authUser");
-
+		
 		//userVo에 세션에 있는 no값 넣기
 		userVo.setNo(authUser.getNo());
 		
-		//userVo 업데이트 후 authUser에 다시 세션값 저장
+		//업데이트
 		userService.modify(userVo);
+		
+		//업데이트 후 userVo no, name 값만 받도록 설정
+		userVo = userService.login(userVo);
+
+		//세션에 저장
 		session.setAttribute("authUser", userVo);
-		
-		System.out.println("업데이트된정보"+userVo);
-		
+		System.out.println(userVo);
 		return "redirect:/main";
 	}
 	
